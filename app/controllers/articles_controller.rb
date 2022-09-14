ffb5c1cb2a02bc14c2fc1f934a16c3dd1a6b17bd
ViktorNAndersen <vn@centic.dk>
2022-09-14 15:41:07 +0200

    Added new article functionality. Still not added to database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,12 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+
+  end
+  def create
+    render plain: params[:article]
+  end
+
 end
