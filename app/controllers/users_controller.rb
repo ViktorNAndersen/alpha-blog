6ded09f57427cffdf9ca0af59069f2eb2e86db70
ViktorNAndersen <vn@centic.dk>
2022-09-22 14:56:08 +0200

    Destroy users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show,:edit,:update]
+  before_action :set_user, only: [:show,:edit,:update, :destroy]
+  before_action :require_user, except: [:index, :show, :new, :create]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     @articles = @user.articles.paginate(page:params[:page], per_page: 2)
@@ -18,7 +20,6 @@ class UsersController < ApplicationController
   end
 
   def update
-
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
@@ -38,11 +39,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles deleted"
+    redirect_to root_path
+  end
+
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit or delete your own articles"
+      redirect_to @user
+    end
   end
 end
