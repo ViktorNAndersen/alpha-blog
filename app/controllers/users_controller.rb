37cea0485a0fddf6c8a599f294b7dfde2c10cffb
ViktorNAndersen <vn@centic.dk>
2022-09-22 12:40:10 +0200

    added a few links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
+      redirect_to @user
     else
       render 'edit'
     end
