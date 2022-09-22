51a4d0de92534a7918741091d854d5d9e18b3212
ViktorNAndersen <vn@centic.dk>
2022-09-22 13:48:33 +0200

    Added logged in users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       flash[:notice] = "Logged in successfully"
       redirect_to user
     else
@@ -17,6 +18,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 end
